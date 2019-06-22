1681ca92e9b88ba663a3f8cabe43217503af1c89
zacharyvunguyen@icloud.com <zacharyvunguyen@icloud.com>
2019-06-22 09:54:50 -0500

    finisn

--- 03_Python/python-challenge/PyPoll/main.py
+++ 03_Python/python-challenge/PyPoll/main.py
@@ -1,36 +1,55 @@
 import csv
-import time 
-import numpy as np
-import pandas as pd
+import time
+import collections as clt
 import os
 
+
 input_file = os.path.join("..","Resources","election_data.csv")
+#input_file = "/Users/zacharyvunguyen/Documents/GitHub/DataAnalytic/03_Python/python-challenge/Resources/election_data.csv"
 start_time = time.time()
 
-csvframe = pd.read_csv(input_file)
+data = csv.DictReader(open(input_file))
 
-#print(csvframe.head())
+cand = []
 
-#access the 'Voter ID' column and call the groupby() function with the Candidate column
-group= csvframe[['Voter ID']].groupby(csvframe['Candidate'])
+for row in data:
+    cand.append(row["Candidate"])
 
-#Counting Vote by Candidate
-countVote= group.count()
-percentVote= group.count()/(csvframe['Voter ID'].count())*100
-print("============= Election Result=============")
+total = len(cand)
 
-print(f" Total Votes: {csvframe['Voter ID'].count()}")
-print("------------------------------------------")
-print (countVote)
-print("------------------------------------------")
-print( percentVote)
-print("------------------------------------------")
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total}")
+print("-------------------------")
 
-final_result=pd.merge(percentVote,countVote,on="Candidate")
-final_result.rename(columns={'Voter ID_x': 'Percentage','Voter ID_y':'Number of Vote'}, inplace=True)
-print(final_result.sort_values(by=['Percentage'],ascending=False))
-#print(type(final_result))
-#print(final_result.sort_values(by=['Percentage'],ascending=False))
-print("------------------------------------------")
-print(countVote.idxmax())
-print(f"Running time: {time.time() - start_time}")
+# dictionary of candidates and their votes
+thisdict = clt.Counter(cand)
+
+ymax = 0
+for x, y in thisdict.items():
+  print(f"{x:<11}: {y/total*100.0:7.3f}%  ({y})")
+  if ymax<y:
+      ymax = y
+      xmax = x
+
+#------------------------
+loopOutput=[]
+ymax = 0
+for x, y in thisdict.items():
+  #print(f"{x:<11}: {y/total*100.0:7.3f}%  ({y})")
+  loopOutput.append(f"{x:<11}: {y/total*100.0:7.3f}%  ({y})")
+  if ymax<y:
+      ymax = y
+      xmax = x
+loopOutput='\n'.join(loopOutput) #add new line to list
+
+print("-------------------------")
+print(f"Winner: {xmax}")
+print("-------------------------")
+
+print("csv.DictReader took %s seconds" % (time.time() - start_time))
+
+text = ((f"Election Results \n-----------------------------\nTotal Votes: {total} \n----------------------------- \n{loopOutput} \n----------------------------- \nWinner: {xmax}   " ))
+saveFile = open('PyPoll.txt', 'w')
+saveFile.write(text)
+saveFile.close()

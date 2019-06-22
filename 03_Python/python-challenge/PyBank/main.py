0e61e9e6962af6b00c0be5622bc82c964d60dd9d
zacharyvunguyen@icloud.com <zacharyvunguyen@icloud.com>
2019-06-22 08:49:21 -0500

    added 'exporting to txt' code

--- 03_Python/python-challenge/PyBank/main.py
+++ 03_Python/python-challenge/PyBank/main.py
@@ -2,13 +2,13 @@ import csv
 import time
 import numpy as np
 import os
-input_file = os.path.join("..","Resources","budget_data.csv")
+
+input_file = os.path.join("..", "Resources", "budget_data.csv")
 start_time = time.time()
 
 data = csv.DictReader(open(input_file))
 
-
-date =[]
+date = []
 pl = []
 
 for row in data:
@@ -26,25 +26,25 @@ print(f"Total: ${np.sum(pl)}")
 minChange = float("inf")
 maxChange = float("-inf")
 sumChange = 0
-interval = date.size-1
+interval = date.size - 1
 for i in range(interval):
-    eachChange = pl[i+1] - pl[i]
+    eachChange = pl[i + 1] - pl[i]
     if (minChange > eachChange):
         minChange = eachChange
-        minChangeDate = date[i+1]
+        minChangeDate = date[i + 1]
     if (maxChange < eachChange):
         maxChange = eachChange
-        maxChangeDate = date[i+1]
+        maxChangeDate = date[i + 1]
 
-    sumChange = sumChange+eachChange
+    sumChange = sumChange + eachChange
 
-
-print(f"Average  Change: ${sumChange/interval}")
+print(f"Average  Change: ${sumChange / interval}")
 print(f"Greatest Increase in Profits: ${maxChangeDate} (${maxChange})")
 print(f"Greatest Decrease in Profits: ${minChangeDate} (${minChange})")
 
-
 print("csv.DictReader took %s seconds" % (time.time() - start_time))
 
-
-
+text = ((f"Financial Analysis \n-----------------------------\nTotal Months: {date.size} \nTotal: ${np.sum(pl)}  \nAverage  Change: ${sumChange / interval} \nGreatest Increase in Profits: ${maxChangeDate} (${maxChange}) \nGreatest Decrease in Profits: ${minChangeDate} (${minChange})" ))
+saveFile = open('PyBank.txt', 'w')
+saveFile.write(text)
+saveFile.close()
